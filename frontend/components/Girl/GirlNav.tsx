ceb7cbc785c45c1e8c4b1671493492cad080064f
George Zhang <georgezhang02@hotmail.com>
2023-10-07 02:50:15 -0500

    Girl dashboard

--- frontend/components/Girl/GirlNav.tsx
+++ frontend/components/Girl/GirlNav.tsx
@@ -35,6 +35,7 @@ export const GirlNav = (props: GirlNavProps) => (
           ),
           tabBarHideOnKeyboard: true,
          }}
+        initialParams={{user: props.user}}
       />
       <Tab.Screen
         name="Lessons"
