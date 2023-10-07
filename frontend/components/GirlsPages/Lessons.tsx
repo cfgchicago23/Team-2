7ee79ed61d05839441d1ace8319877b02a061169
annavan <8annavan@gmail.com>
2023-10-06 23:07:00 -0400

    modal for lessons on click

--- frontend/components/GirlsPages/Lessons.tsx
+++ frontend/components/GirlsPages/Lessons.tsx
@@ -1,31 +1,39 @@
-import React, {useState} from 'react';
-import {View, Text} from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, Modal, Button, StyleSheet } from 'react-native';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import lessonData from './data.json';
 
-const Lessons = () => { 
-  
-    // Define the Module component to display a rectangle with a number, icon, and topic
-  const Module = ({ number, topic }) => (
-    //add on click navigation
-    <View style={styles.moduleContainer}>
-      <View style={styles.rectangle}>
-      <Text style={styles.moduleNumber}>Lesson {number}</Text>
-        <Text style={styles.moduleTopic}>{topic}</Text>
-        <FontAwesome name="star" size={24} color="black" />
-      </View>
-    </View>
-  );
+const Lessons = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedLesson, setSelectedLesson] = useState(null);
 
+  // Define the Module component to display a rectangle with a number, icon, and topic
+  const Module = ({ number, topic }) => {
+    const lesson = lessonData.find((lesson) => lesson.number === number);
+
+    const handleModuleClick = () => {
+      setSelectedLesson(lesson);
+      setModalVisible(true);
+    };
+
+    return (
+      <TouchableOpacity onPress={handleModuleClick}>
+        <View style={styles.moduleContainer}>
+          <View style={styles.rectangle}>
+            <Text style={styles.moduleNumber}>Lesson {number}</Text>
+            <Text style={styles.moduleTopic}>{topic}</Text>
+            <FontAwesome name="star" size={24} color="black" />
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  };
 
   // Map the lessonData to create modules
   const modules = lessonData.map((lesson) => ({
     number: lesson.number,
     topic: lesson.topic, // Add the topic property from the JSON data
   }));
-
 
   return (
     <View style={styles.container}>
@@ -37,12 +45,28 @@ const Lessons = () => {
           topic={module.topic} // Pass the topic as a prop
         />
       ))}
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      {/* EditScreenInfo component */}
+
+      <Modal
+        animationType="slide"
+        transparent={false}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(false);
+        }}
+      >
+        {selectedLesson && (
+          <View style={styles.modalContainer}>
+            <Text style={styles.modalTitle}>Lesson {selectedLesson.number}</Text>
+            <Text style={styles.modalTopic}>{selectedLesson.topic}</Text>
+            <Text style={styles.modalDescription}>{selectedLesson.description}</Text>
+            {/* Add any other information you want to display */}
+            <Button title="Close" onPress={() => setModalVisible(false)} />
+          </View>
+        )}
+      </Modal>
     </View>
   );
-}
-
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -57,11 +81,6 @@ const styles = StyleSheet.create({
     color: 'black',
     marginTop: -150,
   },
-  separator: {
-    marginVertical: 50,
-    height: 1,
-    width: '80%',
-  },
   moduleContainer: {
     alignItems: 'center',
     marginVertical: 10,
@@ -70,10 +89,10 @@ const styles = StyleSheet.create({
     width: 200,
     height: 100,
     borderRadius: 10,
-    backgroundColor: '#DF91C0', // Customize the rectangle's appearance as needed
+    backgroundColor: '#DF91C0',
     alignItems: 'center',
     justifyContent: 'center',
-    marginRight: 20, // Adjust the margin as needed to align the rectangles
+    marginRight: 20,
     marginLeft: 20,
   },
   moduleNumber: {
@@ -81,10 +100,29 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   moduleTopic: {
-    color: 'black', // Change the text color to black
+    color: 'black',
     fontWeight: 'bold',
   },
+  modalContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    padding: 20,
+  },
+  modalTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  modalTopic: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  modalDescription: {
+    fontSize: 16,
+    marginBottom: 20,
+  },
 });
-
 
 export default Lessons;
