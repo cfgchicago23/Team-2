c1e984dd645a962188ac47df74daa135035f2ed3
George Zhang <georgezhang02@hotmail.com>
2023-10-07 00:31:54 -0500

    Removed unnecessary nav from App.tsx

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -10,14 +10,6 @@ import { GirlNav } from './components/Girl/GirlNav';
 import {LeaderNav} from './components/Leader/LeaderNav';
 import { DocumentData } from 'firebase/firestore';
 import { fetchUserData } from './firebase/firestore'
-import {NavigationContainer} from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
-export type tabParamsList = {
-  Home: undefined;
-  Profile: {user: User};
-}
-const Tab = createBottomTabNavigator<tabParamsList>();
 
 export default function App() {
   const [user, setUser] = useState<User>();
